7f2eb46fafde75756d9c818a8a54236ba59f6518
Tchalz <chibuzor_jonathan@yahoomail.com>
2026-09-12 13:09:05 +0100

    Fix hardcoded protocol address and correct mint decimals

--- script/TCH80ZScript.s.sol
+++ script/TCH80ZScript.s.sol
@@ -7,13 +7,14 @@ import {console} from "../lib/forge-std/src/console.sol";
 contract TCH80ZScript is Script {
     string public _name = "TechCrush08";
     string public _symbol = "TCH";
-    address private protocol = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint256 public AmountToMint = 1_000_000e18;
+    address private protocol;
+    uint256 public AmountToMint = 1_000_000e6;
 
     TCH8OZ public tokenTCH;
 
     function run() public {
-        vm.startBroadcast(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        vm.startBroadcast();
+        protocol = tx.origin;
         tokenTCH = new TCH8OZ(_name, _symbol, protocol);
         tokenTCH.mint(protocol, AmountToMint);
         console.log("this is the address of my ERC80Z contract", address(tokenTCH));
